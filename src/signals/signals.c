9e7c2adc26f262767317d44dad162e95038e3614
me3za <medkrix1@gmail.com>
2023-06-16 03:15:08 +0100

    fixed sigint signal to not pass anything to the prompt parser

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 23:56:41 by echoukri          #+#    #+#             */
-/*   Updated: 2023/06/03 20:54:13 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/06/16 01:17:43 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 void	handle_sigint(int sig)
 {
 	(void)sig;
-	ioctl(STDIN_FILENO, TIOCSTI, "\n");
-	rl_replace_line("", 0);
+	write(1, "\n", 1);
 	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 void	redirect_signals(void)
