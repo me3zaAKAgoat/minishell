49a91b9e8075cb17e4f39cff2738693c46c9ffc7
me3za <medkrix1@gmail.com>
2023-06-17 06:55:11 +0100

    fixed passing only a newline as a command line

--- src/parsing/parse.c
+++ src/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 05:10:00 by echoukri          #+#    #+#             */
-/*   Updated: 2023/06/17 03:02:09 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/06/17 05:07:19 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_node	*get_cmds(t_node *tokens)
 	t_node		*cmds;
 
 	cmds = NULL;
-	while (tokens)
+	while (((t_token *)tokens->content)->type != END)
 	{
 		ll_push(&cmds, ll_new(create_command(tokens)));
 		while (((t_token *)tokens->content)->type != END
@@ -43,9 +43,11 @@ void	parse(char *cmd_line)
 		return ;
 	cmds = get_cmds(tokens);
 	if (!cmds)
+	{
+		ll_clear(&tokens, (void *)(void *)clear_token);
 		return ;
+	}
 	execute_commands(cmds);
-	write(1, "\n", 1);
 	ll_clear(&cmds, (void *)(void *)clear_command);
 	ll_clear(&tokens, (void *)(void *)clear_token);
 }
