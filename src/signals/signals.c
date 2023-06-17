e20b5fc3ce51f1ed951c456ab976327e5c86bb88
me3za <medkrix1@gmail.com>
2023-06-17 06:53:57 +0100

    fixed SIGINT for when commands are being executed

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 23:56:41 by echoukri          #+#    #+#             */
-/*   Updated: 2023/06/16 01:17:43 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/06/17 06:52:40 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,22 @@
 void	handle_sigint(int sig)
 {
 	(void)sig;
-	write(1, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+	if (ll_size(g_meta.pids))
+	{
+		while (ll_size(g_meta.pids))
+		{
+			kill((*(pid_t *)g_meta.pids->content), SIGINT);
+			ll_del_one(ll_shift(&g_meta.pids), free);
+		}
+		write(1, "\n", 1);
+	}
+	else
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 
 void	redirect_signals(void)
