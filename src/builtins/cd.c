4311e72240c23724e13c0cf4341dbd378942b435
me3za <medkrix1@gmail.com>
2023-06-22 23:17:11 +0100

    added a few edge cases do git diff

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,21 +6,31 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 02:27:11 by echoukri          #+#    #+#             */
-/*   Updated: 2023/06/20 02:06:00 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/06/22 22:52:10 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	update_env_wd(char *key)
+void	update_env_dirs(char *key)
 {
 	t_dict	*kvp;
 	char	*cwd;
 
 	kvp = get_kvp(g_meta.env, key);
-	free(kvp->value);
-	cwd = getcwd(NULL, 0);
-	kvp->value = ft_strdup(cwd);
+	if (!kvp)
+	{
+		cwd = getcwd(NULL, 0);
+		kvp = new_kvp(key, NULL);
+		kvp->value = ft_strdup(cwd);
+		ll_push(&g_meta.env, ll_new(kvp));
+	}
+	else
+	{
+		free(kvp->value);
+		cwd = getcwd(NULL, 0);
+		kvp->value = ft_strdup(cwd);
+	}
 	free(cwd);
 }
 
@@ -38,9 +48,9 @@ void	cd(char **args)
 	}
 	else
 		dir = args[1];
-	update_env_wd("OLDPWD");
+	update_env_dirs("OLDPWD");
 	if (chdir(dir) == -1)
 		perror("Minishell: cd");
 	else
-		update_env_wd("PWD");
+		update_env_dirs("PWD");
 }
