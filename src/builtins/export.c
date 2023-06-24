a7289b940dab8c34d911e1355b08698ed69f1dce
El Mehdi Kenane <ekenane@e1r9p3.1337.ma>
2023-06-24 18:53:58 +0100

    handle (+=) in export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -15,16 +15,18 @@
 int	key_is_valid(char *key)
 {
 	int	i;
+	int	len;
 
 	if (!key)
 		return (0);
+	len = ft_strlen(key) - 1;
 	i = 0;
 	if (!ft_isalpha(key[i]) && key[i] != '_')
 		return (0);
 	i++;
 	while (key[i])
 	{
-		if (!ft_isalpha(key[i]) && key[i] != '_' && !ft_isdigit(key[i]))
+		if (!ft_isalpha(key[i]) && key[i] != '_' && !ft_isdigit(key[i]) && key[len] != '+')
 			return (0);
 		i++;
 	}
@@ -53,26 +55,56 @@ void	print_export(void)
 	}
 }
 
+void	concatenate_values(t_dict *concatenate_pair, char *new_val)
+{
+	char	*concatenate_val;
+	concatenate_val = ft_strjoin(concatenate_pair->value, new_val);
+	free(concatenate_pair->value);
+	concatenate_pair->value = ft_strdup(concatenate_val);
+	free(concatenate_val);
+}
+
+char	*set_new_value(char **key_value_arr, char **args)
+{
+	char	*new_val;
+
+	new_val = join_arr(key_value_arr + 1, "=");
+	if (ft_strchr(args[1], '=') && !new_val)
+		new_val = ft_strdup("");
+	return (new_val);
+}
+
+void	replace_value(t_dict **existing_pair, char *new_val)
+{
+		free((*existing_pair)->value);
+		(*existing_pair)->value = ft_strdup(new_val);
+}
+
 int	update_env(char **args)
 {
 	char	**key_value_arr;
 	t_dict	*existing_pair;
+	t_dict	*concatenate_pair;
 	char	*new_val;
+	char	*tmp_key;
 
 	key_value_arr = ft_split(args[1], '=');
 	if (!key_is_valid(key_value_arr[0]))
 		return (werror("Minishell: export: `"), werror(args[1]),
 			werror("': not a valid identifier\n"), split_clear(key_value_arr),
 			g_meta.status = BUILTIN_FAIL, -1);
-	new_val = join_arr(key_value_arr + 1, "=");
-	if (ft_strchr(args[1], '=') && !new_val)
-		new_val = ft_strdup("");
+	new_val = set_new_value(key_value_arr, args);
 	existing_pair = get_kvp(g_meta.env, key_value_arr[0]);
+	tmp_key = ft_strtrim(key_value_arr[0], "+");
+	concatenate_pair = get_kvp(g_meta.env, tmp_key);
 	if (existing_pair)
-		(free(existing_pair->value), existing_pair->value = ft_strdup(new_val));
+		replace_value(&existing_pair, new_val);
+	else if (concatenate_pair)
+		concatenate_values(concatenate_pair, new_val);
 	else
-		ll_push(&g_meta.env, ll_new(new_kvp(key_value_arr[0], new_val)));
+		ll_push(&g_meta.env, ll_new(new_kvp(tmp_key, new_val)));
 	free(new_val);
+	free(tmp_key);
 	split_clear(key_value_arr);
 	return (0);
 }
