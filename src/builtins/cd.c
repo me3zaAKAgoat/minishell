4a577ec9e689c5e8e2b78daaac22e6dc6b38950a
El Mehdi Kenane <ekenane@e1r9p3.1337.ma>
2023-06-25 21:32:23 +0100

    handle removed dirs in cd : cd accept complexe arg like ~/Desktop/minishell

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -11,17 +11,39 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <sys/errno.h>
 
-void	update_env_dirs(char *key)
+void	update_env_dirs(char *key, char *dir)
 {
 	t_dict	*kvp;
+	t_dict	*kvp_pwd;
 	char	*cwd;
+	char	*tmp;
 
 	cwd = getcwd(NULL, 0);
 	kvp = get_kvp(g_meta.env, key);
-	if (!kvp)
+	if (!cwd && kvp && !ft_strcmp(key, "PWD"))
 	{
-		kvp->value = ft_strdup(cwd);
+		g_meta.status = 0;
+		perror("cd: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory");
+		tmp = kvp->value;
+		kvp->value = ft_strjoin(tmp, "/");
+		free(tmp);
+		tmp = kvp->value;
+		kvp->value = ft_strjoin(tmp, dir);
+		free(tmp);
+	}
+	else if (!cwd && kvp && !ft_strcmp(key, "OLDPWD"))
+	{
+		kvp_pwd = get_kvp(g_meta.env, "PWD");
+		if (!kvp_pwd)
+			return ;
+		free(kvp->value);
+		kvp->value = ft_strdup(kvp_pwd->value);
+	}
+	else if (!kvp)
+	{
+		kvp = new_kvp(key, ft_strdup(cwd));
 		ll_push(&g_meta.env, ll_new(kvp));
 	}
 	else
@@ -32,14 +54,9 @@ void	update_env_dirs(char *key)
 	free(cwd);
 }
 
-void	too_many_args(void)
-{
-	werror("Minishell: cd: too many arguments\n");
-	g_meta.status = BUILTIN_FAIL;
-}
-
 void	ft_cd(char **args)
 {
+	(void)args;
 	char	*dir;
 	char	*tmp_without_home;
 	char	*tmp_with_home;
@@ -49,28 +66,31 @@ void	ft_cd(char **args)
 	{
 		kvp = get_kvp(g_meta.env, "HOME");
 		if (!kvp)
+		{
+			write (1, "bash: cd: HOME not set", ft_strlen("bash: cd: HOME not set"));
+			write(1, "\n", 1);
+			g_meta.status = BUILTIN_FAIL;
 			return ;
-		if (ft_strcmp(args[1], "~"))
+		}
+		else if (args[1] && ft_strcmp(args[1], "~"))
 		{
 			tmp_without_home = ft_strtrim(args[1], "~");
 			tmp_with_home = ft_strjoin(kvp->value, tmp_without_home);
 			dir = tmp_with_home;
 			free(tmp_without_home);
+			free(tmp_with_home);
 		}
 		else
 			dir = kvp->value;
 	}
-	else if (args[2])
-		return (too_many_args());
 	else
 		dir = args[1];
-	update_env_dirs("OLDPWD");
+	update_env_dirs("OLDPWD", dir);
 	if (chdir(dir) == -1)
 	{
-		free(tmp_with_home);
 		perror("Minishell: cd");
 		g_meta.status = BUILTIN_FAIL;
 	}
 	else
-		update_env_dirs("PWD");
+		update_env_dirs("PWD", dir);
 }
