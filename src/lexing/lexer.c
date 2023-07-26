17cd30884dbdfcb3eb6616b65f75f6f160082142
El Mehdi Kenane <ekenane@e1r9p3.1337.ma>
2023-07-26 14:48:06 +0100

    replace SPACEE with WSPACEE (WHITESPACE) and add tabs as WSPACEE : handle memory leaks

--- src/lexing/lexer.c
+++ src/lexing/lexer.c
@@ -6,7 +6,7 @@
 /*   By: ekenane <ekenane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 03:37:11 by echoukri          #+#    #+#             */
-/*   Updated: 2023/07/24 18:35:07 by ekenane          ###   ########.fr       */
+/*   Updated: 2023/07/26 14:17:52 by ekenane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ static t_token	*get_next_token(char *cmd_line)
 		return (new_token(lex_quotes(cmd_line, '\''), SQUOTE));
 	else if (!ft_strncmp("\"", cmd_line, 1))
 		return (new_token(lex_quotes(cmd_line, '\"'), DQUOTE));
-	else if (!ft_strncmp(" ", cmd_line, 1))
-		return (new_token(ft_strdup(" "), SPACEE));
+	else if (!ft_strncmp(" ", cmd_line, 1) || !ft_strncmp("\t", cmd_line, 1))
+		return (new_token(ft_strdup(" "), WSPACEE));
 	else
 		return (new_token(lex_string(cmd_line), STRING));
 }
@@ -56,41 +56,41 @@ static void	remove_string_quotations(t_node *tokens)
 	}
 }
 
-void	join_two_strings(t_node **current_node, t_node **next_node)
+void	join_two_strings(t_node *current_node)
 {
 	char		*concatenate_value;
-	t_token		*current_token;
-	t_token		*next_token;
+	t_token		*current;
+	t_token		*next;
+	t_node		*tmp;
 
-	current_token = (*current_node)->content;
-	next_token = (*next_node)->content;
-	concatenate_value = ft_strjoin(current_token->value, next_token->value);
-	free(current_token->value);
-	free(next_token->value);
-	free(next_token);
-	(*current_node)->next = (*next_node)->next;
-	current_token->value = concatenate_value;
-	(*current_node)->content = current_token;
+	current = current_node->content;
+	next = current_node->next->content;
+	concatenate_value = ft_strjoin(current->value, next->value);
+	free(current->value);
+	free(next->value);
+	free(next);
+	tmp = current_node->next;
+	current_node->next = current_node->next->next;
+	free(tmp);
+	current->value = concatenate_value;
+	current_node->content = current;
 }
 
 void	join_string_tokens(t_node **tokens)
 {
 	t_node		*current_node;
-	t_node		*next_node;
-	t_token		*current_token;
-	t_token		*next_token;
+	t_token		*current;
+	t_token		*next;
 
 	current_node = (*tokens);
-	next_node = current_node->next;
-	while (next_node)
+	while (current_node->next)
 	{
-		current_token = current_node->content;
-		next_token = next_node->content;
-		if (current_token->type == STRING && next_token->type == STRING)
-			join_two_strings(&current_node, &current_node->next);
+		current = current_node->content;
+		next = current_node->next->content;
+		if (current->type == STRING && next->type == STRING)
+			join_two_strings(current_node);
 		else
 			current_node = current_node->next;
-		next_node = next_node->next;
 	}
 	remove_space_tokens(tokens);
 }
@@ -106,12 +106,12 @@ t_node	*tokenize(char *cmd_line)
 	while (cmd_line[i])
 	{
 		token = get_next_token(cmd_line + i);
+		ll_push(&tokens, ll_new(token));
 		if (token->value == NULL)
 		{
 			return (ll_clear(&tokens, (void *)(void *)clear_token),
 				werror("Unclosed Quotes!\n"), g_meta.status = 1, NULL);
 		}
-		ll_push(&tokens, ll_new(token));
 		i += ft_strlen(token->value);
 	}
 	ll_push(&tokens, ll_new(new_token(NULL, END)));
