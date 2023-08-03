35d76837a79e4e443564ecf5f549637cf2edd6ca
El mehdi Choukri <echoukri@e3r8p6.1337.ma>
2023-08-03 15:21:54 +0100

    small norminette issue

--- src/env/env.c
+++ src/env/env.c
@@ -6,7 +6,7 @@
 /*   By: echoukri <echoukri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 09:38:56 by echoukri          #+#    #+#             */
-/*   Updated: 2023/08/03 15:18:25 by echoukri         ###   ########.fr       */
+/*   Updated: 2023/08/03 15:21:36 by echoukri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ void	free_envp(char **envp)
 void	special_env_init(t_node *head)
 {
 	t_dict	*oldpwd_kvp;
-	
+
 	oldpwd_kvp = new_kvp("OLDPWD", NULL);
 	if (oldpwd_kvp)
 		ll_push(&head, ll_new(oldpwd_kvp));
